934ef7078db256f233d5328d47e811e75e036da0
xiebocong <35874354+xiebocong@users.noreply.github.com>
2018-03-06 11:30:40 -0600

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -5,19 +5,19 @@ library("read.table")
 EPC<- read.table("./EDA_W1/household_power_consumption.txt",  
                  stringsAsFactors = FALSE, sep =";", header=TRUE)
 
-## convert Date class and subset
+## convert to Date class and subset
 EPC$Date <- as.Date(EPC$Date, "%d/%m/%Y")
 subset_EPC <- subset(EPC, Date== "2007-02-01"| Date=="2007-02-02")
-##create column datetime
+## create column datetime by combining Date and Time columns
 subset_EPC$datetime <- strptime(paste(subset_EPC$Date,subset_EPC$Time), 
                                 "%Y-%m-%d %H:%M:%S")
 
-##change variable class to make plot
+## change variables' classes to make plot
 subset_EPC$Sub_metering_1 <- as.numeric(subset_EPC$Sub_metering_1)
 subset_EPC$Sub_metering_2 <- as.numeric(subset_EPC$Sub_metering_2)
 subset_EPC$Sub_metering_3 <- as.numeric(subset_EPC$Sub_metering_3)
 subset_EPC$datetime <- as.POSIXct(subset_EPC$datetime)
-## plot
+## plot step by step(3 intotal), and add annotation(legend) also
 png("plot3.png", width=480, height=480)
 with(subset_EPC,plot(datetime, Sub_metering_1, type="l",xlab="",
                      ylab="Energy sub metering"))

8bfa51d1dd076d2cdd31e09debbf37f26adf2aaa
xiebocong <35874354+xiebocong@users.noreply.github.com>
2018-03-06 11:40:01 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -5,14 +5,14 @@ library("read.table")
 EPC<- read.table("./EDA_W1/household_power_consumption.txt",  
                  stringsAsFactors = FALSE, sep =";", header=TRUE)
 
-## convert Date class and subset
+## convert to Date class and subset
 EPC$Date <- as.Date(EPC$Date, "%d/%m/%Y")
 subset_EPC <- subset(EPC, Date== "2007-02-01"| Date=="2007-02-02")
-##create column datetime
+##create column datetime by combing Date and Time columns
 subset_EPC$datetime <- strptime(paste(subset_EPC$Date,subset_EPC$Time), 
                                 "%Y-%m-%d %H:%M:%S")
 
-##change variable class to make plot
+##change all variables' class to make plot
 subset_EPC$Global_active_power <- as.numeric(subset_EPC$Global_active_power)
 subset_EPC$Global_reactive_power <- as.numeric(subset_EPC$Global_reactive_power)
 subset_EPC$Voltage <- as.numeric(subset_EPC$Voltage)
@@ -22,19 +22,23 @@ subset_EPC$Sub_metering_3 <- as.numeric(subset_EPC$Sub_metering_3)
 subset_EPC$datetime <- as.POSIXct(subset_EPC$datetime)
 ## plot
 png("plot4.png", width=480, height=480)
+# set up layout to make four plots, column-wise
 par(mfcol=c(2,2), mar=c(4,4,1,1))
+# figure1
 with(subset_EPC,plot(datetime, Global_active_power, type="l",xlab="",
                      ylab="Global Active Power"))
-
+# figure2
 with(subset_EPC,plot(datetime, Sub_metering_1, type="l",xlab="",
                      ylab="Energy sub metering"))
 lines(subset_EPC$datetime,subset_EPC$Sub_metering_2, col="red")
 lines(subset_EPC$datetime,subset_EPC$Sub_metering_3, col="blue")
 legend("topright", lty=1, col=c("black", "red", "blue"),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
-       ,cex=0.7,bty="n")
+       ,cex=0.7,bty="n") # remove border and zoom out 
+#figure3
 with(subset_EPC,plot(datetime, Voltage, type="l",xlab="datetime",
                      ylab="Voltage"))
+#figure4
 with(subset_EPC,plot(datetime, Global_reactive_power, type="l",xlab="datetime",
                      ylab="Global_reactive_power"))
 dev.off()
